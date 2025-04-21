4ed8cc5fe64d3c9a317e879947760fff67b34e97
hamaken <kenshin.hamada@rakus-partners.co.jp>
2025-04-21 19:38:57 +0900

    登録画面におけるバリデーションを追加しました

--- demo/src/main/java/com/example/demo/controller/UserRegisterController.java
+++ demo/src/main/java/com/example/demo/controller/UserRegisterController.java
@@ -3,9 +3,13 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import com.example.demo.form.RegisterForm;
 import com.example.demo.service.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,12 +29,22 @@ public class UserRegisterController {
         model.addAttribute("registerForm", new RegisterForm());
         return "register";
     }
-    
+
     @PostMapping("/register")
+    public String registerUser(@Valid RegisterForm registerForm, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        model.addAttribute("message", "登録が完了しました");
+        return "redirect:/register-success";
+    }
+    
+    /*@PostMapping("/register")
     public String registerUser(@ModelAttribute RegisterForm form) {
         userService.registerUser(form);
         return "redirect:/register-success";
-    }
+    }*/
 
     @GetMapping("/register-success")
     public String showRegisterSuccessPage() {
--- demo/src/main/java/com/example/demo/form/RegisterForm.java
+++ demo/src/main/java/com/example/demo/form/RegisterForm.java
@@ -1,11 +1,24 @@
 package com.example.demo.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class RegisterForm {
 
+    @NotBlank(message = "名前を入力してください")
+    @Size(max = 20, message = "名前は20字以内で入力してください")
     private String name;
 
+    @NotBlank(message = "メールアドレスを入力してください")
+    @Email(message = "メールアドレスが不正です")
+    @Size(max = 100, message = "メールアドレスは100字以内で入力してください")
     private String email;
 
+    @NotBlank(message = "パスワードを入力してください")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "半角英数で入力してください")
+    @Size(min = 4, max = 20, message = "パスワードは4文字以上20字以内、半角英数で入力してください")
     private String password;
 
     public String getName() {
